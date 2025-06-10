bab1d8023fc9ea0966e4c3c7a3b2b203fdc48268
Joanna <joanna.zg@gmail.com>
2025-06-10 21:13:39 +0300

    Event

--- plants/models.py
+++ plants/models.py
@@ -35,3 +35,16 @@ class Plant(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     species = models.ForeignKey(Species, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+
+class Event(models.Model):
+    EVENT_CHOICE= [
+    ('water', 'Water'),
+    ('fertilize', 'Fertilize'),
+    ('repot', 'Repot'),
+    ('death', 'Plant Died'),
+    ]
+
+    plant=models.ForeignKey(Plant,on_delete=models.PROTECT, null=False, blank=False)
+    date =models.DateField(verbose_name="תאריך",null=False, blank=False)
+    event_type = models.CharField(max_length=10, choices=EVENT_CHOICE)
+    user= models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
